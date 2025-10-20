5e4816aebe197808c057440fff612629fc67e244
vamshikrish04 <vkrishna.garrepally04@gmail.com>
2025-10-19 18:56:32 -0700

    changes are made

--- Tour_pics/Vamshi.cs
+++ Tour_pics/Vamshi.cs
@@ -1,5 +1,14 @@
 using System;
 using System.Web.Services.Description;
 
-Console.WriteLine(value: "Hello World");
-Console.WriteLine(value: "char");
+namespace Tour_Management
+{
+    public class vamshi
+    {
+        public static void Mian()
+        {
+            Console.WriteLine(value: "Hello World");
+            Console.WriteLine(value: "char");
+        }
+    }
+}
